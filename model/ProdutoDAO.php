67bd0a7d591b572c8b980989a5bbd6d7055e8188
Sahelluis21 <Sahelluis77@gmail.com.br>
2025-08-30 19:10:06 -0300

    Mudança de variaveis e name do arq

---
+++ model/ProdutoDAO.php
@@ -0,0 +1,62 @@
+<?php
+require_once "Banco.php";
+require_once "Cliente.php";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+class ProdutoDAO {
+    private $conexao;
+
+    public function __construct() {
+        $this->conexao = (new Banco())->conexao;
+    }
+
+    public function listar() {
+        $stmt = $this->conexao->prepare("SELECT * FROM produto");
+        $stmt->execute();
+        $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $produto = [];
+        foreach($dados as $d) {
+            $produto[] = new Produto($d['codigo'], $d['descricao'], $d['preco'], $d['qtde']);
+        }
+
+        return $produto;
+    }
+
+    public function buscarPorId($codigo) {
+        $stmt = $this->conexao->prepare("SELECT * FROM produto WHERE codigo = :codigo");
+        $stmt->bindParam(':codigo', $codigo, PDO::PARAM_INT);
+        $stmt->execute();
+        $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($dados) {
+            return new Produto($dados['codigo'], $dados['descricao'], $dados['preco'], $dados['qtde']);
+        }
+        return null;
+    }
+
+    public function inserir(Produto $produto) {
+        $stmt = $this->conexao->prepare("INSERT INTO produto (descricao, preco, qtde) VALUES (:descricao, :preco, :qtde)");
+        $stmt->bindValue(':descricao', $produto->getdescricao());
+        $stmt->bindValue(':preco', $produto->getpreco());
+        $stmt->bindValue(':qtde', $produto->getqtde(), PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function atualizar(Produto $produto) {
+        $stmt = $this->conexao->prepare("UPDATE produto SET descricao = :descricao, preco = :preco, qtde = :qtde WHERE codigo = :codigo");
+        $stmt->bindValue(':nome', $cliente->getNome());
+        $stmt->bindValue(':email', $cliente->getEmail());
+        $stmt->bindValue(':id', $cliente->getId(), PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function remover($id) {
+        $stmt = $this->conexao->prepare("DELETE FROM clientes WHERE id = :id");
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+}
